680ddf1e81b0d1c3ebceff0d7de404d0d4332682
Mehdony <86969900+Mehdony@users.noreply.github.com>
2022-07-29 20:22:07 +0200

    Affichage des medias de l'utilisateur

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -21,6 +21,7 @@
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
+    
     };
 
     async function init() {
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,14 @@
+import {mediaFactory} from "../factories/media.js"
 //Mettre le code JavaScript lié à la page photographer.html
 const urlRequest = window.location.search;
 const urlSearchParams = new URLSearchParams(urlRequest);
 const userId = urlSearchParams.get("id");
-console.log(userId);
+// console.log(userId);
 
 fetch("../data/photographers.json").then((res) => {
   res.json().then((res) => {
     const photographersList = res;
-    console.log(photographersList.photographers);
+    // console.log(photographersList.photographers);
     const photographer = photographersList.photographers.find(
       (photographer) => photographer.id == userId
     );
@@ -22,5 +23,39 @@ fetch("../data/photographers.json").then((res) => {
     location.innerText = `${photographer.city}, ${photographer.country}`;
     quote.innerText = photographer.tagline;
     pp.setAttribute("src", ` ../assets/photographers/${photographer.portrait}`);
+
+
+    // Récupérations des medias d'un photographe(correspondant a userId)
+      const medias = photographersList.media.filter(
+        (media) => media.photographerId == userId
+      )
+      console.log(medias);
+
+    
+    // Affichage du model et des datas
+    async function displayData(medias) {
+      const mediasSection = document.querySelector(".medias_section");
+    
+
+      medias.forEach((media) => {
+        // Envoie des medias vers la factory
+          const mediaModel = mediaFactory(media);
+          // Construction de la carte media grâce aux données envoyées à la factory 
+          const mediaCardDOM = mediaModel.getMediaCardDOM();
+          // Ajout de la carte a media_section 
+          mediasSection.appendChild(mediaCardDOM);
+      });
+    }
+
+
+    async function init() {
+    // Récupère les datas des photographes
+    // const { medias } = await get(); 
+    displayData(medias);
+    };
+    
+    init();
+
   });
 });
+
